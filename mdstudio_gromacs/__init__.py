5197df594d12a3a17b812ef9b2e8f47eefaa59ea
Marc van Dijk <marcvdijk@gmail.com>
2019-11-26 14:34:29 +0100

    Add package meta data variables

--- mdstudio_gromacs/__init__.py
+++ mdstudio_gromacs/__init__.py
@@ -1,1 +1,18 @@
 # -*- coding: utf-8 -*-
+
+"""
+MDStudio gromacs provides GROMACS Molecular Dynamics fnctionality
+"""
+
+import os
+
+__module__ = 'mdstudio_gromacs'
+__docformat__ = 'restructuredtext'
+__version__ = '{major:d}.{minor:d}'.format(major=0, minor=2)
+__author__ = 'Marc van Dijk'
+__status__ = 'pre-release beta1'
+__date__ = '15 april 2016'
+__licence__ = 'Apache Software License 2.0'
+__url__ = 'https://github.com/MD-Studio/MDStudio_gromacs'
+__copyright__ = "Copyright (c) VU University, Amsterdam"
+__rootpath__ = os.path.dirname(__file__)
