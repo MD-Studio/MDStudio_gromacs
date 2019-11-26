2ee495156b66e72fe64742f51236b5fb4f8a3e72
Marc van Dijk <marcvdijk@gmail.com>
2019-11-26 16:02:11 +0100

    Dynamically add package to path if needed

--- mdstudio_gromacs/__main__.py
+++ mdstudio_gromacs/__main__.py
@@ -1,5 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
 from mdstudio.runner import main
-from mdstudio_gromacs.wamp_services import MDWampApi
+
+# Try import package
+try:
+    from mdstudio_gromacs.wamp_services import MDWampApi
+
+except ImportError:
+
+    # Add modules in package to path
+    modulepath = os.path.abspath(os.path.join(os.path.dirname(__file__), '../'))
+    if modulepath not in sys.path:
+        sys.path.insert(0, modulepath)
+
+    from mdstudio_gromacs.wamp_services import MDWampApi
+
 
 if __name__ == '__main__':
     main(MDWampApi)
